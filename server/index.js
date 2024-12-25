9fe8a265fb9c9700aa1ae239bd5fca868d764d84
Mitul Vs Code <mitulmovaliya@gmail.com>
2024-12-25 22:28:33 +0530

    Cookie issues

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: ["http://localhost:5173"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    // methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
